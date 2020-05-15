dae31b85d42a81b5a1048625baf18bdcf9414fb0
Noah Nethery <nethery.n@gmail.com>
2020-05-15 13:04:42 -0500

    removed test

--- test.py
+++
@@ -1,9 +0,0 @@
-import gym, time, random
-import numpy as np
-from envs.raindrops_gym import RaindropsGym
-
-env = RaindropsGym()
-
-while True:
-    env.step_frame(env.action_space.sample())
-    time.sleep(1/60)
