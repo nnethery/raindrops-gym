5c87324a711f90dd2e705464386791e2c0652623
Noah Nethery <nethery.n@gmail.com>
2020-05-15 12:00:49 -0500

    removed old observation space flattening class

--- envs/raindrops_gym.py
+++ envs/raindrops_gym.py
@@ -2,13 +2,15 @@ import gym
 from gym import error, spaces, utils
 import numpy as np
 import subprocess, time
+from raindrops_python.game import Game
 
 
 class RaindropsGym(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, jar_location, pixels):
-        
+    def __init__(self, show_score = False):
+        self.show_score = show_score
+        self.game = Game()
 
     def step(self, action):
         if action == 0: # NO OP
@@ -45,26 +47,3 @@ class RaindropsGym(gym.Env):
         if self.viewer is not None:
             self.viewer.close()
             self.viewer = None
-
-class RaindropsWrapper(gym.ObservationWrapper):
-    """This wrapper converts a Box observation into a single integer.
-    """
-    def __init__(self, env, n_bins=10, low=None, high=None):
-        super().__init__(env)
-        assert isinstance(env.observation_space, spaces.Box)
-
-        low = self.observation_space.low if low is None else low
-        high = self.observation_space.high if high is None else high
-
-        self.n_bins = n_bins
-        self.val_bins = [np.linspace(l, h, n_bins + 1) for l, h in
-                         zip(low.flatten(), high.flatten())]
-        self.observation_space = spaces.Discrete(n_bins ** low.flatten().shape[0])
-
-    def _convert_to_one_number(self, digits):
-        return sum([d * ((self.n_bins + 1) ** i) for i, d in enumerate(digits)])
-
-    def observation(self, observation):
-        digits = [np.digitize([x], bins)[0]
-                  for x, bins in zip(observation.flatten(), self.val_bins)]
-        return self._convert_to_one_number(digits)
